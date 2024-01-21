5456ac5cb767d4063a6e1c375ae8520fff06a071
Toyin-Fadiran <50837407+Toyin-Fadiran@users.noreply.github.com>
2024-01-21 12:20:44 -0600

    second commit

---
+++ src/main/java/com/example/estore/EStoreDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStoreDockerApplication.class, args);
+	}
+
+}
